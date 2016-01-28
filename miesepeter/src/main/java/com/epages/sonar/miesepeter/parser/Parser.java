801e2fab8684d816350a2524c33c38395e48f73a
Oliver Trosien <otrosien@epages.com>
2016-01-28 10:23:43 +0100

    Logging impl.

--- miesepeter/src/main/java/com/epages/sonar/miesepeter/parser/Parser.java
+++ miesepeter/src/main/java/com/epages/sonar/miesepeter/parser/Parser.java
@@ -1,7 +1,11 @@
 package com.epages.sonar.miesepeter.parser;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.epages.sonar.miesepeter.parser.issues.GenericTle;
 import com.epages.sonar.miesepeter.parser.issues.JavaScriptInTemplate;
@@ -10,9 +14,22 @@ import com.epages.sonar.miesepeter.parser.issues.LoopIssues;
 
 
 public class Parser {
-	public ParseResult parseFile(File file) {
-		List<CodeLine> linedFile = FileRunner.loadFile(file);
 
+	private static final Logger log = LoggerFactory.getLogger(Parser.class);
+
+	public ParseResult parseFile(File file) {
+		try {
+			log.info("parsing file {}", file);
+			List<CodeLine> linedFile = FileRunner.loadFile(file);
+			return parseCodeLines(linedFile);
+		} catch (IOException e) {
+			log.error("cannot parse file {}", file, e);
+		}
+		log.warn("returning empty parse result for file {}", file);
+		return new ParseResult();
+	}
+
+	private ParseResult parseCodeLines(List<CodeLine> linedFile) {
 		ParseResult result = new ParseResult();
 		result.setLonelySet( new LonelySet().parse(linedFile) );
 		result.setGenericTle(new GenericTle().parse(linedFile));
